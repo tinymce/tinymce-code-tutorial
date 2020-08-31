ef78f07ac8754ad33ac5c47e0d2c076d5caadacc
Dylan Just <>
2020-08-31 13:57:39 +1000

    Code style exercise.

--- src/main/ts/1CodeStyle.ts
+++ src/main/ts/1CodeStyle.ts
@@ -1,3 +1,5 @@
+import { VisualViewport } from '@ephox/sugar';
+
 /*
 Code Style
 ==========
@@ -23,13 +25,34 @@ interface Boundz {
 
 Notice the use of the arrow function - we like arrow functions because they're concise.
 We tell tsc to transpile to ES5, so IE works.
+
+Notice also that we have an explicit return type. This lets the compiler check that our
+code matches the type signature.
 */
-export const width = (b: Boundz) =>
+export const width = (b: Boundz): number =>
   /* TODO */ -1;
 
 // TODO implement height function
 
 /*
-At this point, run `tsc` at your shell to make sure everything compiles.
+At this point, run `yarn tsc` at your shell to make sure everything compiles.
+
+3. This structure is similar to a structure in our Sugar DOM manipulation library,
+so let's see if we can convert between the two.
 */
 
+type VVBounds = VisualViewport.Bounds;
+
+// TODO
+// export const toSugarBounds = (b: Boundz): VVBounds => ({
+//
+// });
+
+// TODO: implement fromSugarBounds
+
+/*
+Ok, so we started off pretty easy.
+
+Now, code is useless without tests, so let's head over to 1CodeStyleTest.ts
+and let's write some tests.
+*/
