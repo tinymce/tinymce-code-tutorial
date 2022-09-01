e0770c20862bb2fc346839d4e94fae92a6b78808
Mike Zhu <mkzh.org@gmail.com>
2022-09-01 13:37:27 +1000

    Add Part 2 Ex2

--- src/main/ts/Part2Ex2ArrayFunctions.ts
+++ src/main/ts/Part2Ex2ArrayFunctions.ts
@@ -1,4 +1,4 @@
-import { Arr, Optional } from '@ephox/katamari';
+import { Arr, Optional } from "@ephox/katamari";
 
 /*
 Katamari is our library for general-purpose functions and FP basics.
@@ -9,8 +9,8 @@ Its array module "Arr" is very handy, so let's explore it.
 We don't write loops if we can help it. Instead, we go up a level, and call functions that do the looping for us.
 The simplest of these is 'each' which just iterates.
 
-TODO: Run the following code using this command:
-yarn bedrock-auto -b chrome-headless -f src/test/ts/Exercise2ArrayFunctionsTest.ts
+DONE: Run the following code using this command:
+yarn bedrock-auto -b chrome-headless -f src/test/ts/part2/Exercise2ArrayFunctionsTest.ts
  */
 
 export const runEach1 = (): void => {
@@ -33,14 +33,16 @@ export interface Frog {
 }
 
 export const myFrogs: Frog[] = [
-  { name: 'frog1', ribbits: true, age: 3 },
-  { name: 'frog2', ribbits: false, age: 4 },
-  { name: 'loudfrog', ribbits: true, age: 1 },
-  { name: 'quietfrog', ribbits: false, age: 10 },
+  { name: "frog1", ribbits: true, age: 3 },
+  { name: "frog2", ribbits: false, age: 4 },
+  { name: "loudfrog", ribbits: true, age: 1 },
+  { name: "quietfrog", ribbits: false, age: 10 },
 ];
 
 export const runEach2 = (): void => {
-  // TODO: Use Arr.each and console.log to print the name of each frog
+  Arr.each(myFrogs, (frog) => {
+    console.log(frog.name);
+  });
 };
 
 /*
@@ -56,20 +58,19 @@ Let's go through some examples of using Arr.map, then see if you can get the fro
  */
 
 // add 2 to each element
-export const runMap1 = (xs: number[]): number[] =>
-  Arr.map(xs, (x) => x + 2);
+export const runMap1 = (xs: number[]): number[] => Arr.map(xs, (x) => x + 2);
 
 // prepend a string to each element
 export const runMap2 = (xs: number[]): string[] =>
   Arr.map(xs, (x) => "the number is " + x);
 
-// TODO: Return the frog's names and check it by running
-// yarn bedrock-auto -b chrome-headless -f src/test/ts/Exercise2ArrayFunctionsTest.ts
-export const frogNames = (fs: Frog[]): string[] =>
-  [];
+// DONE: Return the frog's names and check it by running
+// yarn bedrock-auto -b chrome-headless -f src/test/ts/part2/Exercise2ArrayFunctionsTest.ts
+export const frogNames = (fs: Frog[]): string[] => Arr.map(fs, (f) => f.name);
 
-// TODO: Return the frog's ages
-// TODO: Write a test for this in Exercise2ArrayFunctionsTest
+// DONE: Return the frog's ages
+export const frogAges = (fs: Frog[]): number[] => Arr.map(fs, (f) => f.age);
+// DONE: Write a test for this in Exercise2ArrayFunctionsTest
 
 /*
 4. Arr.filter
@@ -81,14 +82,14 @@ e.g. to get all the even numbers out of a list:
 export const evens = (xs: number[]): number[] =>
   Arr.filter(xs, (x) => x % 2 === 0);
 
-// TODO: Write a function that returns all the frogs that ribbit
-// TODO: Run the provided test to check your answer.
+// DONE: Write a function that returns all the frogs that ribbit
+// DONE: Run the provided test to check your answer.
 export const ribbitting = (frogs: Frog[]): Frog[] =>
-  [];
+  Arr.filter(frogs, (frog) => frog.ribbits);
 
-// TODO: Write a function that returns all frogs aged 8 or older
+// DONE: Write a function that returns all frogs aged 8 or older
 export const olderFrogs = (frogs: Frog[]): Frog[] =>
-  [];
+  Arr.filter(frogs, (frog) => frog.age >= 8);
 
 /*
 5. Arr.exists
@@ -96,10 +97,13 @@ export const olderFrogs = (frogs: Frog[]): Frog[] =>
 Arr.exists returns true if there is one or more element that matches a predicate.
  */
 
-// TODO: Write a function that returns true if there's one or more ribbiting frogs
+// DONE: Write a function that returns true if there's one or more ribbiting frogs
+export const oneOrMoreRibbiting = (frogs: Frog[]): boolean =>
+  Arr.exists(frogs, (frog) => frog.ribbits);
 
-// TODO: Write a function that takes an array of numbers, and returns true if there are any negative numbers
-
+// DONE: Write a function that takes an array of numbers, and returns true if there are any negative numbers
+export const hasNegativeNumber = (xs: number[]): boolean =>
+  Arr.exists(xs, (x) => x < 0);
 /*
 6. Arr.bind
 
@@ -108,11 +112,10 @@ This results in an array of arrays, which is then flattened.
 
 This behaviour of running map then flatten is why this function is sometimes called "flatmap".
 
-TODO: Write a function that takes a list of strings, each string containing a comma-separated list of values, and returns all of the values as an array.
+DONE: Write a function that takes a list of strings, each string containing a comma-separated list of values, and returns all of the values as an array.
  */
 export const splitCsvs = (csvs: string[]): string[] =>
-  [];
-
+  Arr.bind(csvs, (csv) => csv.split(","));
 /*
 7. Arr.find
 
--- src/test/ts/part2/Exercise2ArrayFunctionsTest.ts
+++ src/test/ts/part2/Exercise2ArrayFunctionsTest.ts
@@ -1,38 +1,86 @@
-import { describe, it } from '@ephox/bedrock-client';
-import { assert } from 'chai';
-import * as Ex from '../../../main/ts/Part2Ex2ArrayFunctions';
+import { describe, it } from "@ephox/bedrock-client";
+import { assert } from "chai";
+import * as Ex from "../../../main/ts/Part2Ex2ArrayFunctions";
 
-describe('Exercise2ArrayFunctionsTest', () => {
-  it('runEach1', () => {
+describe("Exercise2ArrayFunctionsTest", () => {
+  it("runEach1", () => {
     Ex.runEach1();
   });
 
-  it('frog names', () => {
-    assert.deepEqual(Ex.frogNames(Ex.myFrogs), [ 'frog1', 'frog2', 'loudfrog', 'quietfrog' ], 'frog names');
+  it("frog names", () => {
+    assert.deepEqual(
+      Ex.frogNames(Ex.myFrogs),
+      ["frog1", "frog2", "loudfrog", "quietfrog"],
+      "frog names"
+    );
   });
 
-  it('frog ages', () => {
-    // TODO: write a test for your frog ages function
+  it("frog ages", () => {
+    assert.deepEqual(Ex.frogAges(Ex.myFrogs), [3, 4, 1, 10]);
   });
 
-  it('ribbitting frogs', () => {
-    assert.deepEqual(Ex.ribbitting(Ex.myFrogs), [
-      { name: 'frog1', ribbits: true, age: 3 },
-      { name: 'loudfrog', ribbits: true, age: 1 }
-    ], 'ribbitting');
+  it("ribbitting frogs", () => {
+    assert.deepEqual(
+      Ex.ribbitting(Ex.myFrogs),
+      [
+        { name: "frog1", ribbits: true, age: 3 },
+        { name: "loudfrog", ribbits: true, age: 1 },
+      ],
+      "ribbitting"
+    );
   });
 
-  it('older frogs', () => {
-    assert.deepEqual(Ex.olderFrogs(Ex.myFrogs), [
-      { name: 'quietfrog', ribbits: false, age: 10 }
-    ], 'older frogs');
+  it("older frogs", () => {
+    assert.deepEqual(
+      Ex.olderFrogs(Ex.myFrogs),
+      [{ name: "quietfrog", ribbits: false, age: 10 }],
+      "older frogs"
+    );
   });
 
-  it('csvs', () => {
-    assert.deepEqual(Ex.splitCsvs([]), [], 'empty array');
-    assert.deepEqual(Ex.splitCsvs(['a']), ['a'], 'single string');
-    assert.deepEqual(Ex.splitCsvs(['a,b']), ['a', 'b'], 'single csv string');
-    assert.deepEqual(Ex.splitCsvs(['a', 'b']), ['a', 'b'], 'several strings');
-    assert.deepEqual(Ex.splitCsvs(['a', 'b,d,a']), ['a', 'b', 'd', 'a'], 'several csv strings');
+  it("one or more ribbits", () => {
+    assert.deepEqual(
+      Ex.oneOrMoreRibbiting(Ex.myFrogs),
+      true,
+      "one or more ribbits"
+    );
+
+    const nonRibbitFrogs = [
+      { name: "frog1", ribbits: false, age: 3 },
+      { name: "frog2", ribbits: false, age: 4 },
+      { name: "loudfrog", ribbits: false, age: 1 },
+      { name: "quietfrog", ribbits: false, age: 10 },
+    ];
+    assert.deepEqual(
+      Ex.oneOrMoreRibbiting(nonRibbitFrogs),
+      false,
+      "no ribbits"
+    );
+  });
+
+  it("has negative number", () => {
+    assert.deepEqual(
+      Ex.hasNegativeNumber([1, 2, 3, 4]),
+      false,
+      "no negative number"
+    );
+
+    assert.deepEqual(
+      Ex.hasNegativeNumber([-1, 2, 3, 4]),
+      true,
+      "has negative number"
+    );
+  });
+
+  it("csvs", () => {
+    assert.deepEqual(Ex.splitCsvs([]), [], "empty array");
+    assert.deepEqual(Ex.splitCsvs(["a"]), ["a"], "single string");
+    assert.deepEqual(Ex.splitCsvs(["a,b"]), ["a", "b"], "single csv string");
+    assert.deepEqual(Ex.splitCsvs(["a", "b"]), ["a", "b"], "several strings");
+    assert.deepEqual(
+      Ex.splitCsvs(["a", "b,d,a"]),
+      ["a", "b", "d", "a"],
+      "several csv strings"
+    );
   });
 });
